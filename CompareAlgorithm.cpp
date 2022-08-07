0de688fbd309506bb2c06d0b8e4898776115cdb2
Andrew Deryabin <andrew@danvd.me>
2022-08-07 23:05:00 +0400

    Fix issues. Add Makefile (for building in linux)

--- CompareAlgorithm.cpp
+++ CompareAlgorithm.cpp
@@ -19,16 +19,37 @@ namespace spellchecker
 		for (j = 0; j <= len_other; j++) {
 			m_dist[j][0] = j;
 		}
+		bool was_insertion = false;
+		bool was_deletion = false;
 		for (j = 1; j <= len_word; j++) {
 			for (i = 1; i <= len_other; i++) {
 				if (word[j - 1] == other[i - 1]) {
 					track = 0;
 				}
 				else {
-					track = 1;
+					// Substitution is not allowed by task conditions
+					track = MAX_WORD_LENGTH + 1;
 				}
-				t = MIN((m_dist[i - 1][j] + 1), (m_dist[i][j - 1] + 1));
-				m_dist[i][j] = MIN(t, (m_dist[i - 1][j - 1] + track));
+				auto delete_cost = m_dist[i - 1][j] + 1;
+				auto insert_cost = m_dist[i][j - 1] + 1;
+				t = std::min(delete_cost, insert_cost);
+				if (delete_cost < insert_cost) {
+					if (was_deletion) {
+						t = MAX_WORD_LENGTH + 1; // Adjacent deletions are not allowed by task conditions
+					}
+					was_insertion = false;
+					was_deletion = true;
+				} else if (insert_cost < delete_cost) {
+					if (was_insertion) {
+						t = MAX_WORD_LENGTH + 1; // Adjacent insertions are not allowed by task conditions
+					}
+					was_deletion = false;
+					was_insertion = true;
+				} else {
+					was_deletion = was_insertion = false;
+				}
+
+				m_dist[i][j] = std::min(t, m_dist[i - 1][j - 1] + track);
 			}
 		}
 		return m_dist[len_other][len_word];
